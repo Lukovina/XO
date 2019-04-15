eb8d0101871b54a02e3701e30731e1f46139e70d
Volodymyr Lukovyna <vladimir.lukovina@gmail.com>
2019-04-15 19:11:38 +0300

    few changes to Application props

--- src/Game.js
+++ src/Game.js
@@ -4,7 +4,7 @@ import Sprite from './helpers/Sprite.js';
 // The application will create a renderer using WebGL, if possible,
 // with a fallback to a canvas render. It will also setup the ticker
 // and the root stage PIXI.Container
-const app = new PIXI.Application();
+const app = new PIXI.Application({width: window.innerWidth, height:window.innerHeight});
 
 // The application will create a canvas element for you that you
 // can then insert into the DOM
